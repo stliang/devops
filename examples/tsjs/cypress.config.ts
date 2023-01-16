55b806d3a219d6e46d3d94bebeaa33f1d83b6ea2
Steve Liang <steve.liang@roche.com>
2023-01-15 21:04:05 -0800

    basic angular cypress project init code

---
+++ examples/tsjs/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress'
+import * as registerCodeCoverageTasks from '@cypress/code-coverage/task';
+
+export default defineConfig({
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config);
+      return config;
+    },
+    baseUrl: 'http://localhost:4200',
+  },
+
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts'
+  }
+
+})
