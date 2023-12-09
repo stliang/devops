131ecf2e1c07c0eadbd9f9e5559222b77f53d4c5
stliang <stliang@gmail.com>
2023-12-09 13:00:35 -0800

    adding SIGN Term signal handling

--- demo/raspberry_pi/pi_sensor_service.py
+++ demo/raspberry_pi/pi_sensor_service.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import io
+import signal
+import socket
 import sys
 import fcntl
 import time
@@ -13,19 +15,55 @@ import paho.mqtt.client as mqtt
 
 MQTT_BROKER_URL = "192.168.1.104"
 MQTT_PUBLISH_TOPIC = "/sensors/CLE/v1/device5/pH"
+MQTTC = mqtt.Client("pH sensor")
+
+# Broker reachability test parameters
+port = 80
+retry = 5
+delay = 10
+timeout = 3
+
+def isOpen(ip, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        try:
+                s.connect((ip, int(port)))
+                s.shutdown(socket.SHUT_RDWR)
+                return True
+        except:
+                return False
+        finally:
+                s.close()
+
+def checkHost(ip, port):
+        ipup = False
+        for i in range(retry):
+                if isOpen(ip, port):
+                        ipup = True
+                        break
+                else:
+                        time.sleep(delay)
+        return ipup
+
+def on_log(client, userdata, level, buf):
+    print(f"log: {buf}")
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(f"Disconnected flags: {flags} return code: {rc}")
+    client.connected_flag=False
 
 def on_connect(client, userdata, flags, rc):
     if rc==0:
-        client.connected_flag=True #set flag
+        client.connected_flag=True
         print("connected OK")
     else:
         print("Bad connection Returned code=",rc)
         client.bad_connection_flag=True
 
-def on_disconnect(client, userdata, rc):
-    logging.info("disconnecting reason  "  +str(rc))
-    client.connected_flag=False
-    client.disconnect_flag=True
+def sigterm_handler(_signo, _stack_frame):
+    MQTTC.loop_stop()
+    MQTTC.disconnect()
+    sys.exit(0)
 
 def get_devices():
     device = AtlasI2C()
@@ -46,39 +84,56 @@ def get_devices():
        
 def main():
     
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
     mqtt.Client.connected_flag=False
 
     mqtt.Client.bad_connection_flag=False
-
-    device_list = get_devices()
-        
-    device = device_list[0]
     
     # delaytime = 300  # 5 minutes
 
     delaytime = 1.5 # seconds
 
+    # TODO read run_flag from env var
+    run_flag = True
+
     try:
-        mqttc = mqtt.Client()
-        mqttc.on_connect=on_connect
-        mqttc.connect(MQTT_BROKER_URL)
-        mqttc.loop_start()
-        while not mqttc.connected_flag and not mqttc.bad_connection_flag:
-            for dev in device_list:
-                dev.write("R")
-            time.sleep(delaytime)
-            for dev in device_list:
-                value = dev.read()
-                mqttc.publish(MQTT_PUBLISH_TOPIC, value)
-                print(value)
-        if mqttc.bad_connection_flag:
-            client.loop_stop()    #Stop loop
-            sys.exit()
-        mqttc.disconnect()
-    except:
-        print("General exception")
+        MQTTC.on_log=on_log
+        MQTTC.on_connect=on_connect
+        MQTTC.on_disconnect=on_disconnect
+        # Check broker is reachable otherwise connection failure will exit
+        if checkHost(MQTT_BROKER_URL, port):
+            print(f"{MQTT_BROKER_URL} is UP")
+        # As a service, it is better to start the loop before connecting
+        MQTTC.loop_start()
+        MQTTC.connect(MQTT_BROKER_URL)
+        while not MQTTC.connected_flag and not MQTTC.bad_connection_flag:
+            print("Waiting for MQTT connection")
+            time.sleep(1)
+            if MQTTC.bad_connection_flag:
+                MQTTC.loop_stop()
+                sys.exit(1)
+        try:
+            while run_flag:
+                device_list = get_devices()
+                device = device_list[0]
+                for dev in device_list:
+                    dev.write("R")
+                time.sleep(delaytime)
+                for dev in device_list:
+                    value = dev.read()
+                    MQTTC.publish(MQTT_PUBLISH_TOPIC, value)
+                    print(value)
+                # run_flag = read from env var
+                # delaytime = read from env var
+                # MQTT_PUBLISH_TOPIC = read from env var
+        except Exception as e:
+            print(f"i2c or mqtt error\n{e}")
+        MQTTC.loop_stop()
+        MQTTC.disconnect()
+    except Exception as e:
+        print(f"Exception:\n{e}")
         sys.exit(1)
 
-                    
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
